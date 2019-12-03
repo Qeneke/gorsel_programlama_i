f9d34f56f27589f45d3bb8198698cc079bd96128
Burak Shen <tobuba2@gmail.com>
2019-12-03 06:46:03 +0300

    add output

--- 5.py
+++ 5.py
@@ -1,5 +1,12 @@
 import envs  # envs modülümüzü içeri aktarıyoruz (import)
 print(str.maketrans("a", "A"))
-print(''.maketrans(envs.kaynak, envs.hedef))
-# envs.kaynak="şçöğüıŞÇÖĞÜİ"
+# --ÇIKTI--
+#{97: 65}
+# --ÇIKTI--
+
 # envs.hedef="scoguiSCOGUI"
+# envs.kaynak="şçöğüıŞÇÖĞÜİ"
+print(''.maketrans(envs.kaynak, envs.hedef))
+# --ÇIKTI--
+# {351: 115, 231: 99, 246: 111, 287: 103, 252: 117, 305: 105, 350: 83, 199: 67, 214: 79, 286: 71, 220: 85, 304: 73}
+# --ÇIKTI--
